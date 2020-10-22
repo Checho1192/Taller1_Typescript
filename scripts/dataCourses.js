7873f5bf37d6356c24117c4baae11bf670c20a04
Sergio Sierra <sd.sierra@uniandes.edu.co>
2020-10-21 19:25:07 -0500

    Taller completo

--- scripts/dataCourses.js
+++ scripts/dataCourses.js
@@ -1,6 +1,5 @@
 import { Course } from './course.js';
-
-export const dataCourses = [
+export var dataCourses = [
     new Course("Desarrollo de SW en Equipos", "Sergio Yodeb Velásquez", 3),
     new Course("Sistemas Transaccionales", "Germán Bravo", 3),
     new Course("Diseño y Análisis de Algoritmos", "Jorge Duitama", 3),
@@ -9,4 +8,4 @@ export const dataCourses = [
     new Course("Física Experimental II", "Diego Ramirez", 1),
     new Course("CBU: El sentido de la educación", "Jorge Andres Mejia", 2),
     new Course("CBU: Comportamiento y Cerebro Humano", "Veronika Akle", 2)
-]
+];
